22df6984a5c466b3396cbfa5cef45978408a7777
Artem Jarovoj <artemjarovoj17@googlemail.com>
2025-11-29 02:15:36 +0100

    add copy to clipboard by tapping on message snackbar

--- lib/common/utils/exception_message.dart
+++ lib/common/utils/exception_message.dart
@@ -8,17 +8,10 @@ class ExceptionMessage {
         behavior: SnackBarBehavior.floating,
         backgroundColor: Colors.transparent,
         elevation: 0,
+        duration: const Duration(seconds: 5),
         content: GestureDetector(
           onTap: () {
             Clipboard.setData(ClipboardData(text: message));
-
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: const Text("Kopiert!"),
-                duration: const Duration(seconds: 1),
-                behavior: SnackBarBehavior.floating,
-              ),
-            );
           },
           child: Container(
             padding: const EdgeInsets.all(12),
@@ -31,19 +24,17 @@ class ExceptionMessage {
               children: [
                 const Icon(Icons.error_outline, color: Colors.white, size: 26),
                 const SizedBox(width: 12),
-
-                // 🔥 Text block is flexible and supports unlimited lines
                 Expanded(
-                  child: SelectableText(
+                  child: Text(
                     message,
                     style: const TextStyle(color: Colors.white, fontSize: 16),
+                    softWrap: true,
                   ),
                 ),
               ],
             ),
           ),
         ),
-        duration: const Duration(seconds: 5),
       ),
     );
   }
