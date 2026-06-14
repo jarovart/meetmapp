18596689cd74a369786ac58a09a13cb11db03b32
Artem Jarovoj <artemjarovoj17@googlemail.com>
2026-06-14 09:49:29 +0200

    add devconfig

--- lib/app/config/api_config.dart
+++ lib/app/config/api_config.dart
@@ -1,36 +1,9 @@
-import 'package:flutter/foundation.dart';
-import 'dart:io' show Platform;
+import 'package:meetmaap/app/config/dev_config.dart';
 
 class ApiConfig {
-  static const int _port = 8080;
-  static const String _dev = String.fromEnvironment('dev', defaultValue: '');
+  static const String prodUrl = 'https://freemoment.de';
 
   static String get baseUrl {
-    debugPrint("api base url: $_dev");
-    return (_dev.isNotEmpty) ? baseUrl1 : 'https://freemoment.de';
-  }
-
-  static String get baseUrl1 {
-    // 🌐 Flutter Web
-    if (kIsWeb) {
-      return 'http://localhost:$_port';
-    }
-
-    // 📱 Mobile / Desktop
-    if (Platform.isAndroid) {
-      // Android Emulator
-      return 'http://10.0.2.2:$_port';
-    }
-
-    if (Platform.isWindows) {
-      return 'http://127.0.0.1:$_port';
-    }
-
-    if (Platform.isIOS || Platform.isMacOS) {
-      return 'http://localhost:$_port';
-    }
-
-    // ❌ Fallback (sollte nie passieren)
-    throw UnsupportedError('Unsupported platform');
+    return (DevConfig.isDev) ? DevConfig.devUrl : prodUrl;
   }
 }
---
+++ lib/app/config/dev_config.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class DevConfig {
+  static const bool isDev = false;
+  static const int _port = 8080;
+
+  static String get devUrl {
+    // 🌐 Flutter Web
+    if (kIsWeb) {
+      return 'http://localhost:$_port';
+    }
+
+    // 📱 Mobile / Desktop
+    if (Platform.isAndroid) {
+      // Android Emulator
+      return 'http://10.0.2.2:$_port';
+    }
+
+    if (Platform.isWindows) {
+      return 'http://127.0.0.1:$_port';
+    }
+
+    if (Platform.isIOS || Platform.isMacOS) {
+      return 'http://localhost:$_port';
+    }
+
+    // ❌ Fallback (sollte nie passieren)
+    throw UnsupportedError('Unsupported platform');
+  }
+}
